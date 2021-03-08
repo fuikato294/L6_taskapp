f79425033257b2364bf08f2768b8a87ca9efe457
Fumitaka.Kotou <fumikato294@gmail.com>
2021-03-08 10:08:30 +0900

    Initial Commit

---
+++ L6 taskapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  L6 taskapp
+//
+//  Created by fumitaka katou on 2021/03/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
