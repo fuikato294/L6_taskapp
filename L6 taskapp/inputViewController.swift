03a5acefdbf34f34481158c040130e83bcf345e6
Fumitaka.Kotou <fumikato294@gmail.com>
2021-03-08 11:36:14 +0900

    pod installと.gitignoreの作成まで実施

---
+++ L6 taskapp/inputViewController.swift
@@ -0,0 +1,33 @@
+//
+//  inputViewController.swift
+//  L6 taskapp
+//
+//  Created by fumitaka katou on 2021/03/08.
+//
+
+import UIKit
+
+class inputViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+           
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '14.2'
+
+target 'L6 taskapp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for L6 taskapp
+  pod 'RealmSwift','10.1.4'
+
+end
